8f4724cb8e15656393f87f89d0fa5be90cfef96c
singi.qin <singi.qin@quctel.com>
2026-01-08 17:51:24 +0800

    feat: 新增首页动画效果

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,31 @@
-import type {ReactNode} from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import type { ReactNode } from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Heading from "@theme/Heading";
+import FloatingLines from "@site/src/components/FloatingLines";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+    <header
+      className={clsx("hero hero--primary", styles.heroBanner)}
+      style={{ backgroundColor: "transparent", color: "white" }}
+    >
+      <div
+        style={{
+          width: "100%",
+          height: "calc(100vh - 48px)",
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
@@ -20,9 +33,30 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/intro"
+          >
             Docusaurus Tutorial - 5min ⏱️
           </Link>
+        </div>
+
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            left: 0,
+            top: 0,
+          }}
+        >
+          <FloatingLines 
+          enabledWaves={['top', 'bottom']}
+          lineCount={[10, 20]}
+          lineDistance={[8, 4]}
+          bendRadius={5.0}
+          bendStrength={-0.5}
+          interactive={true}
+          parallax={true}
+        />
         </div>
       </div>
     </header>
@@ -30,11 +64,12 @@ function HomepageHeader() {
 }
 
 export default function Home(): ReactNode {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
